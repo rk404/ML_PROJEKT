472aa4c662bc54637481be5ebad4b7becd16db79
Rafal Kaminski <rafalkaminski6@gmail.com>
2025-11-04 16:27:17 +0100

    wylaczenie weryfikacji ssl

--- data_import.py
+++ data_import.py
@@ -1,27 +1,32 @@
-import requests
+﻿import requests
 import pandas as pd
 from io import StringIO
+import urllib3
+
+# Wyłącz ostrzeżenia o niezweryfikowanym SSL
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def load_onedrive_csv(share_url):
-    """
+    '''
     Wczytuje CSV z publicznego linku OneDrive bezpośrednio do DataFrame
-    """
+    '''
     # Dodaj parametr download do URL
     if '?' in share_url:
         download_url = share_url + '&download=1'
     else:
         download_url = share_url + '?download=1'
 
-    print("Pobieranie danych z OneDrive...")
-    response = requests.get(download_url)
+    print('Pobieranie danych z OneDrive...')
+    # Wyłącz weryfikację SSL (verify=False)
+    response = requests.get(download_url, verify=False)
 
     if response.status_code == 200:
         # Dekoduj content i wczytaj do pandas
         csv_content = response.content.decode('utf-8')
         df = pd.read_csv(StringIO(csv_content), sep=';')
-        print(f"Wczytano {len(df)} wierszy i {len(df.columns)} kolumn")
+        print(f'Wczytano {len(df)} wierszy i {len(df.columns)} kolumn')
         return df
     else:
-        print(f"Błąd pobierania: {response.status_code}")
+        print(f'Błąd pobierania: {response.status_code}')
         return None
--- static_ev.py
+++ static_ev.py
@@ -1,2 +1,8 @@
-kombinacje_url = "https://1drv.ms/x/c/8E1FC8AC1722703E/EWTt4HgC3xNItsgAxOL2w3kBqsaS3duIJzIleeU279QTKA?e=573HeJ"
-indeksy_url = "https://1drv.ms/x/c/8E1FC8AC1722703E/EUQ4_AMx3uNGiJn9x_BOMl0BcYX8xNYpNmBYGuEPf2I5kQ?e=mCfmgt"
+kombinacje_url = (
+    "https://1drv.ms/x/c/8E1FC8AC1722703E/"
+    "EWTt4HgC3xNItsgAxOL2w3kBqsaS3duIJzIleeU279QTKA?e=573HeJ"
+)
+indeksy_url = (
+    "https://1drv.ms/x/c/8E1FC8AC1722703E/"
+    "EUQ4_AMx3uNGiJn9x_BOMl0BcYX8xNYpNmBYGuEPf2I5kQ?e=mCfmgt"
+)
