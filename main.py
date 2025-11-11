0f77070043f3ddcb93714ee3698ce2356d9b81a2
DAMIAN GUTOWSKI <damianvongutowski@gmail.com>
2025-11-11 16:59:14 +0100

    tiktoken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import static_ev as ev
 from data_import import load_onedrive_csv
+from tokenizer import load_tiktoken
 
 kombinacje_df = load_onedrive_csv(ev.kombinacje_url)
 indeksy_df = load_onedrive_csv(ev.indeksy_url)
@@ -7,3 +8,9 @@ print("Kombinacje DataFrame:")
 print(kombinacje_df.head())
 print("Indeksy DataFrame:")
 print(indeksy_df.head())
+enc = load_tiktoken("o200K_base")  # domyślnie o200K_base, można zmienić
+text =  indeksy_df.iloc[0]['indeks']  # Załóżmy, że jest kolumna 'text_column'
+tokens = enc.encode(text)
+print("Tekst z indeksów:", text)
+print("Tokeny:", tokens)
+decoded = enc.decode(tokens)
